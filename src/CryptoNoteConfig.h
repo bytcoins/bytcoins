af850f51350003fc87b82ace48cb9ffb81995a0d
Gonzalo Linares Amezcua <escreativa@escreativa.com>
2017-08-04 16:21:13 +0200

    Update CryptoNoteConfig.h

--- src/CryptoNoteConfig.h
+++ src/CryptoNoteConfig.h
@@ -106,9 +106,7 @@ const char     P2P_STAT_TRUSTED_PUB_KEY[]                    = "8f80f9a5a434a9f1
 
 //TODO Add here your network seed nodes
 const std::initializer_list<const char*> SEED_NODES = {
-  "85.214.124.161:17236",
-  "93.188.165.101:17236",
-  "31.220.54.79:17236",
+  "156.67.218.73:17236",
   //"your_seed_ip2.com:8080",
 };
 
